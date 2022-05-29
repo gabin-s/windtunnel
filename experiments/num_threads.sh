78d32e6559b1714eb5a33e9f7cbdd48f922b900a
Gabin Schieffer <gabin.schieffer@gmail.com>
2022-05-29 11:18:23 +0200

    added an experiment

---
+++ experiments/num_threads.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+N_RUNS=5
+EXEC_PATH='./wind_omp'
+
+max_iter=1000
+threshold=-1 # threshold=-1 to prevent early stopping
+inlet_pos=0
+inlet_size=100
+rows=5000
+cols=5000
+
+args="$rows $cols $max_iter $threshold $inlet_pos $inlet_size 0 0 0 0 0 0 123 456 789"  # no particles
+
+echo "# running $EXEC_PATH, with <nthreads> thread(s)"
+echo "# args: $args"
+
+for nthreads in 1 2 4 8 16 32 64 128 256; do
+    printf "%d" $nthreads
+
+    export OMP_NUM_THREADS=$nthreads
+
+    for _ in $(seq $N_RUNS); do
+        time_l=$( echo $args | xargs "$EXEC_PATH" | grep "Time: ")
+
+        printf " ${time_l#Time: }"
+    done
+    echo
+done
